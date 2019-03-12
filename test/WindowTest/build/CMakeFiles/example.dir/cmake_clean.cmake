dd7793d34559db54568e840cc307a51108c6d5b6
Utkarsh-Khambra <utkarshqw@gmail.com>
2019-03-12 22:53:00 +0530

    test folder for window apis

---
+++ test/WindowTest/build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/test.cpp.o"
+  "CMakeFiles/example.dir/home/raven/BaseRenderer/src/Window.cpp.o"
+  "example.pdb"
+  "example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
