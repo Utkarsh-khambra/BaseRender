9324248403bd5b014ff7b40bfbf79284b9a23eff
Utkarsh-Khambra <utkarshqw@gmail.com>
2019-03-12 21:06:28 +0530

    Window defination files

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <string>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <exception>
+#define NDEBUG
+#include <cassert>
+#include "../include/Window.hpp"
 
 
 Window::Window (int width_, int height_, std::string title_):width(width_),
                                                              height(height_),
-                                                             title(title_) {}
+                                                             title(title_)
+{
+    if (width < 0)
+        throw std::invalid_argument("Width is negative");
+    if (height < 0)
+        throw std::invalid_argument("Height is negative");
+}
 void Window::intialize ()
 {
+    assert(windowptr == NULL);
     if (!glfwInit()) {
         glfwTerminate();
         throw std::runtime_error("GLFW failed to initialzie");
@@ -15,12 +27,13 @@ void Window::intialize ()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    windowptr = glfwCreateWindow (width, height, title, NULL, NULL);
+    windowptr = glfwCreateWindow (width, height, title.c_str(), NULL, NULL);
 }
 
 void Window::makeCurrent ()
 {
-    glfwMakeCurrent(windowptr);
+    assert(windowptr != NULL);
+    glfwMakeContextCurrent(windowptr);
 
     if (glewInit() != GLEW_OK) {
         glfwTerminate();
@@ -30,5 +43,18 @@ void Window::makeCurrent ()
 
 void Window::update ()
 {
+    glEnable(GL_DEPTH_TEST);
+    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Window::resize ()
+{
+
+}
+
+Window::~Window ()
+{
+    glfwDestroyWindow(windowptr);
+    glfwTerminate();
 
 }
